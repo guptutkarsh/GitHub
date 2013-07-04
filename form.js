864f1ab110d8732f25ea377c66ff2b77188e8753
Utkarsh Gupta <guptutkarsh@gmail.com>
2013-07-04 07:57:33 +0530

    some changes

---
+++ form.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var a=0,b;
+function take(){
+	b=document.getElementById("name").value;
+	if(document.getElementById("name").value=='')
+	{a=1;
+	window.alert("enter d name");
+	}if(document.getElementById("pwd").value=='')
+	{a=1;window.alert("enter d password");
+	}
+	if(a==0)
+	{var win1=window.open("hostel.html");
+		}}
+	
--- hostel.js
+++ hostel.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+var a=0;
+function clicked(){var y=window.opener.document.getElementById('name').value;
+document.getElementById("para").innerHTML+=y;
+document.getElementById("para1").innerHTML+=y;
+}
+function change(x){if(a==1)
+window.alert("already blocked 1 room");
+else
+{a++;
+window.alert("you have blocked "+x);
+document.getElementById("para1").innerHTML+=" has blocked "+x;
+}}
