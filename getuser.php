27ed6769e63f67e1b263090a8d361c6af3ba68ad
Utkarsh Gupta <guptutkarsh@gmail.com>
2013-07-06 21:42:45 +0530

    data feedback

---
+++ getuser.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<?php
+mysql_connect("localhost","root","") or die (mysql_error());
+mysql_select_db("utkarsh") or die(mysql_error());
+
+$sql="SELECT * FROM user WHERE id = 'yes'";
+$result = mysql_query($sql);
+while($row = mysql_fetch_array($result))
+  {
+  echo $row['room no']." ";
+  echo "<br />";
+  }
+
+  
+?>
+<body>
+</body>
+</html>
--- hostel.js
+++ hostel.js
@@ -1,13 +1,36 @@
-// JavaScript Document
+var y=new Array();
+  var c=0;
+var inputs=new Array();
 var a=0;
+var b=new Array();
+var n=new Array();
+function avail(){ n=document.getElementById("txt").textContent;
+	 b=n.split(" ");
+	  for(d=1;d<b.length-1;d++)
+	 {y[c]=b[d];
+	 c++;}
+	
+ inputs = document.getElementsByTagName("input");
+for (var i = 0; i < inputs .length; i++) 
+{if(inputs[i].id==y[2])
+alert("hjjbj");
+}}
+
 function clicked(){var y=window.opener.document.getElementById('name').value;
 document.getElementById("para").innerHTML+=y;
 document.getElementById("para1").innerHTML+=y;
 }
-function change(x){if(a==1)
+function change(x){
+	
+	if(a==1)
 window.alert("already blocked 1 room");
 else
 {a++;
 window.alert("you have blocked "+x);
 document.getElementById("para1").innerHTML+=" has blocked "+x;
 }}
+
+
+
+	
+	
